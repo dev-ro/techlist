817510e59c549db0cd54c950f1585ce63db53367
Kyle Debro <debro@umich.edu>
2024-07-20 23:53:00 -0400

    updates

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,8 @@ COPY . /app
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Make port 8501 available to the world outside this container
-EXPOSE 8501
+# Make port 8080 available to the world outside this container
+EXPOSE 8080
 
-# Run streamlit app
-CMD ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
+# Run streamlit app on port 8080
+CMD ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
--- app.py
+++ app.py
@@ -6,21 +6,94 @@ from collections import Counter
 import altair as alt
 import numpy as np
 
+# Set page configuration
+st.set_page_config(
+    page_title="techlist.me",  # Title of the web page
+    page_icon="🖥️",  # Favicon (optional), can be an emoji or a path to an image file
+    layout="centered",  # Use "wide" or "centered" layout
+    menu_items={
+        "About": """
+        ## Techlist.me
+        This application provides insights into the technology stack and skill requirements for data-related job postings.
+        Created by Kyle as part of the Master of Applied Data Science program at the University of Michigan School of Information.
+        """
+    },
+)
+"""
+# Techlist.me
+
+Welcome to [techlist.me](https://techlist.me/#dashboard)! This platform provides valuable insights into the technology stack and skill requirements across various data-related job postings. My name is Kyle, and I created this platform to help job seekers and employers alike understand the most in-demand technologies and skills in the data industry. This project is also part of my capstone project for the Master of Applied Data Science program at the University of Michigan School of Information.
+
+## Introduction
+
+In the rapidly evolving tech industry, staying updated with the latest trends and in-demand skills is crucial for both job seekers and employers. Techlist.me is designed to aggregate and analyze job postings to provide a clear picture of the current landscape in data-related roles.
+
+## Data Sources
+
+The data presented on Techlist.me is collected from various job boards and company websites. The focus is on extracting key information such as:
+
+- **Job Titles**: Positions related to data science, machine learning, data engineering, and more.
+- **Hard Skills**: Technical skills that are explicitly required in job postings.
+- **Soft Skills**: Interpersonal and non-technical skills valued by employers.
+- **Tech Stack**: Programming languages, frameworks, tools, and technologies in demand.
+- **Industries**: Sectors that are actively hiring for data-related roles.
+- **Companies**: Organizations with a significant number of job postings in the data domain.
+- **Benefits**: Common benefits offered to attract top talent.
+- **Salaries**: Compensation ranges to provide insights into market trends.
+
+## Features
+
+- **Interactive Dashboards**: Visualize data through interactive charts and graphs.
+- **Keyword Search**: Filter job postings by specific keywords to find relevant insights.
+- **Trend Analysis**: Track the popularity of skills and technologies over time.
+- **Company Insights**: Identify top hiring companies and the skills they prioritize.
+
+## How to Use
+
+1. **Select a Job Title Keyword**: Use the dropdown menu to choose a specific job title or select "All Data Related Jobs" to view aggregate data.
+2. **Explore Visualizations**: View the top 20 in-demand hard skills, tech stack, soft skills, industries, and benefits for the selected job title.
+3. **Analyze Salaries**: Get insights into average minimum and maximum salaries for the selected job title.
+4. **Company Exclusions**: Note that certain companies (recruiters and consultants) have been excluded to ensure the accuracy of the data.
+
+## Conclusion
+
+Techlist.me aims to provide actionable insights to help job seekers align their skills with market demand and assist employers in understanding current hiring trends. I hope you find this platform useful and informative.
+
+For any feedback or inquiries, please contact me at [support@techlist.me](mailto:support@techlist.me).
+
+---
+
+### Disclaimer
+
+The data presented on Techlist.me is based on job postings and may not fully represent the entire job market. I continuously strive to improve data collection and analysis methods to provide the most accurate and up-to-date information.
+
+---
+"""
+
+
 # Directory containing the JSON files
 directory = "data/extracted/gemini"
 
-# List to hold all job entries
-all_jobs = []
 
-# Loop through the files and read each JSON file
-for filename in os.listdir(directory):
-    if filename.endswith(".json"):
-        with open(os.path.join(directory, filename), "r", encoding="utf-8") as file:
-            jobs = json.load(file)
-            all_jobs.extend(jobs)
+@st.cache_data
+def load_data():
 
-# Convert the list of dictionaries to a DataFrame
-data = pd.DataFrame(all_jobs)
+    # List to hold all job entries
+    all_jobs = []
+    # Loop through the files and read each JSON file
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            with open(os.path.join(directory, filename), "r", encoding="utf-8") as file:
+                jobs = json.load(file)
+                all_jobs.extend(jobs)
+
+    # Convert the list of dictionaries to a DataFrame
+    data = pd.DataFrame(all_jobs)
+    return data
+
+
+data = load_data()
+data = data.dropna(subset=["summary"])
 
 
 # Function to replace words in lists
@@ -61,6 +134,14 @@ replacements = {
     "analytical thinking": "Analytical",
     "analytical skills": "Analytical",
     "Presentation": "Presentation Skills",
+    "apache spark": "Spark",
+    "apache kafka": "Kafka",
+    "apache hadoop": "Hadoop",
+    "apache airflow": "Airflow",
+    "azure databricks": "Databricks",
+    "google cloud platform": "GCP",
+    "amazon web services": "AWS",
+    "aws services": "AWS",
 }
 
 # Replace words in the specified columns
@@ -107,7 +188,7 @@ data = data[~data["company"].isin(blacklist_companies)]
 data["created_on"] = pd.to_datetime(data["created_on"], unit="s")
 
 # Streamlit app layout
-st.title("Techlist.me Dashboard")
+st.title("Dashboard")
 
 # Get the unique keywords for the drop-down menu and add "All"
 unique_keywords = ["All Data Related Jobs"] + data["keyword"].unique().tolist()
@@ -123,6 +204,7 @@ else:
 
 
 # Function to count frequency of words in lists, case-insensitively
+@st.cache_data
 def count_frequency(column_data):
     all_items = []
     for item in column_data:
@@ -134,6 +216,7 @@ def count_frequency(column_data):
 
 
 # Function to calculate median min and max salary
+@st.cache_data
 def calculate_mean_salary(data):
     min_salaries = [
         entry["min"]
@@ -286,3 +369,27 @@ st.write(
     f"The following companies have been excluded from the analysis:\n{excluded_companies}"
 )
 st.write(f"Number of excluded job postings: {excluded_jobs_count}")
+
+
+"""
+
+## Data Table
+
+"""
+filtered_data[
+    [
+        "created_on",
+        "keyword",
+        "location",
+        "company",
+        "title",
+        "summary",
+        "url",
+        "hard_skills",
+        "tech_stack",
+        "soft_skills",
+        "industries",
+        "salary",
+        "benefits",
+    ]
+]
