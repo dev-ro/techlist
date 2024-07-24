4c69c1079c6efcbf82fdf4e0a4060d683dd3c7ce
Kyle Debro <debro@umich.edu>
2024-07-24 19:07:16 -0400

    update python version in containers

--- location.py
+++ location.py
@@ -1,7 +1,4 @@
 # TODO script to update locations for extracted_data.jobs
-# TODO table to store blacklisted companies
-# TODO script to remove blacklist companies from database
-# TODO update collect_job_listings.py to automatically skip all blacklisted companies in table
 
 # TODO query to see remaining jobs to be extracted
 # TODO BigQuery link for extracted_data.jobs to share with team
